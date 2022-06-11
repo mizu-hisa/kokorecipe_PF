c4ad0db8973c0782e0cf26b1a17c18e4c8ea40d0
mizu-hisa <101858095+mizu-hisa@users.noreply.github.com>
2022-06-11 12:41:27 +0000

    ユーザー情報編集更新

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -6,5 +6,21 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
   end
+
+  def update
+    @customer = Customer.find(current_customer.id)
+    @customer.update(customer_params)
+    redirect_to customer_path(current_customer.id)
+  end
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:user_name, :profile_image)
+  end
+
+
 end

4abc1fe1354abdf7494a8562fee21a8414f1bc54
mizu-hisa <101858095+mizu-hisa@users.noreply.github.com>
2022-06-11 11:46:27 +0000

    ユーザーページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,25 @@
 Rails.application.routes.draw do
 
+  # 顧客用
+  # devise_for :customers
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # devise_for :admins
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
   scope module: :public do
     get '/' => 'homes#top'
     resources :recipes, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+    resources :customers, only: [:show, :edit]
   end
-
-
-# 顧客用
-# devise_for :customers
-# URL /customers/sign_in ...
-devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-# 管理者用
-# devise_for :admins
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-
-
-
-
-
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

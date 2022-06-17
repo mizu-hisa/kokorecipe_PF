41765041095cfea625c60dabb934abaaa28306f6
mizu-hisa <albicocca.m.1031@gmail.com>
2022-06-17 05:24:07 +0000

    テストデプロイ途中3

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+requir 'uglifier'
 
 module Kokorecipe
   class Application < Rails::Application
